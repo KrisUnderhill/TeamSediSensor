ccec40222ad644d7fae8f9fb643b4089ebd4f9e9
Kris <kristophermiedema@gmail.com>
2022-01-04 21:32:41 -0500

    Added libs and rakefile integration

---
+++ Software/test_HWintegration/Code/Libs/Task_Measure/Task_Measure.h
@@ -0,0 +1,18 @@
+#ifndef TASK_MEASURE_H_
+#define TASK_MEASURE_H_
+
+#include "Arduino.h"
+
+class TaskMeasure {
+    public: 
+        TaskMeasure(){};
+        void init();
+        void run();
+    private:
+        const int POT_PIN = 34;
+        const int LED_PIN = 22;
+        int potReading;
+};
+
+#endif /* TASK_MEASURE_H_ */
+
