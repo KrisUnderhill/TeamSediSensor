159674909eabf4a8feb1320098963a0121ccc403
Kris <kristophermiedema@gmail.com>
2022-02-25 14:01:24 -0500

    Drive LED off PWM Signal
    Using LEDC lib from ESP to drive LED on pwm signal
    Rakefile changed to use Embedded flash for memory

--- Software/prod/Code/Libs/Task_Measure/Task_Measure.cpp
+++ Software/prod/Code/Libs/Task_Measure/Task_Measure.cpp
@@ -5,13 +5,25 @@
 
 #include "Arduino.h"
 #include "Task_Measure.h"
+
+#include "driver/ledc.h" /* ~/.arduino15/packages/esp32/hardware/esp32/1.0.6/tools/sdk/include/driver/driver */
 #include "sys/time.h"
 #include "stdio.h"
 #include <time.h>
+
 #include "../Config/config.h" /* Include path for lib */
 #include "../PS_FileSystem/PS_FileSystem.h"
 /* #include timer stuff - done automatically by Arduino IDE */
+
 #define MSG_LEN 500
+#define LEDC_TIMER              LEDC_TIMER_1
+#define LEDC_MODE               LEDC_LOW_SPEED_MODE
+#define LEDC_OUTPUT_IO          LED_PIN
+#define LEDC_CHANNEL            LEDC_CHANNEL_0
+#define LEDC_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
+#define LEDC_DUTY_ON            (8190) // Set duty to 50%. ((2 ** 13) - 1) * 50% = 4095
+#define LEDC_DUTY_OFF           (0) // Set duty to 0%.
+#define LEDC_FREQUENCY          (5000) // Frequency in Hertz. Set frequency at 5 kHz
 
 volatile bool TaskMeasure::taskRunning = true;
 int TaskMeasure::activeReading = 0;
@@ -26,8 +38,8 @@ void TaskMeasure::init(){
     PS_FileSystem::init();
 
     /* setup pins */
-    pinMode(LED_PIN, OUTPUT);
     pinMode(PHOTOTRANSISTOR_PIN, OUTPUT);
+
     /* Time setup - hacked from SimpleTime example and idf docs*/
     setenv("TZ", "EST+5EDT,M3.2.0/2,M11.1.0/2", 1); /* hardcoded eastern */
     tzset();
@@ -41,6 +53,29 @@ void TaskMeasure::init(){
     timerAttachInterrupt(timer, &TimerISR, true); /* attach ISR to interrupt */
     timerAlarmWrite(timer, 5000000, true); /* Timer fires every 5,000,000 us (5s), repeat true */
     timerAlarmEnable(timer); /* Enable timer alarms */
+
+    /* setup LEDC to control LED */
+    ledc_timer_config_t ledc_timer = {
+        LEDC_MODE,
+        LEDC_DUTY_RES,
+        LEDC_TIMER,
+        LEDC_FREQUENCY,  // Set output frequency at 5 kHz
+    };
+
+    ledc_timer_config(&ledc_timer);
+
+    // Prepare and then apply the LEDC PWM channel configuration
+    ledc_channel_config_t ledc_channel = {
+        .gpio_num       = LEDC_OUTPUT_IO,
+        .speed_mode     = LEDC_MODE,
+        .channel        = LEDC_CHANNEL,
+        .intr_type      = LEDC_INTR_DISABLE,
+        .timer_sel      = LEDC_TIMER,
+        .duty           = 0, // Set duty to 0%
+        .hpoint         = 0
+    };
+
+    ledc_channel_config(&ledc_channel);
 }
 
 void TaskMeasure::run(){
@@ -52,7 +87,8 @@ void TaskMeasure::run(){
             darkReading = analogRead(ADC_PIN);
 
             /* turn on LED */
-            digitalWrite(LED_PIN, HIGH);
+            ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY_ON);
+            ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
             /* wait 0.5 sec */
             delay(1500);
             /* take reading */
@@ -63,7 +99,9 @@ void TaskMeasure::run(){
             /* Wait 0.5 sec */
             delay(500);
             /* turn LED off */
-            digitalWrite(LED_PIN, LOW);
+            ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY_OFF);
+            ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+
             tempAdc = analogRead(TEMP_PIN);
 
             /* Format string */
